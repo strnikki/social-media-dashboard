6754568c359339a54774cca5ec0a2fc0ecc0da21
Nicole Fanelli <nicolefanelli@protonmail.com>
2020-09-07 20:17:22 -0300

    Implement the theme changing switch

---
+++ script.js
@@ -0,0 +1,26 @@
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'light') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light'); //add this
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); //add this
+    }    
+}
+
+
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
